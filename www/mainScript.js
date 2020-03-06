6c07ec359de09706e5e2c585e6260f9b0c854cc4
gires1 <61874986+gires1@users.noreply.github.com>
2020-03-06 14:20:22 +0100

    Inital Version

---
+++ www/mainScript.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    var a = 2;
+    var b = 3;
+
+    var c = a + b;
+    var x = 5;
+    var y = 2;
+    var z = x - y;
+    var x = 5;
+    var z = Math.pow(x, 2);
+
+    function showValueInElement(id, label, value) {
+        var labelElement = window.document.getElementById(id+"label");
+        if (labelElement) {
+            labelElement.textContent = label;
+        }
+        var valueElement = window.document.getElementById(id);
+        if (valueElement) {
+            valueElement.textContent = value;
+        }
+    }
+    setTimeout(function () {
+        showValueInElement("input1", "x:", x);
+        showValueInElement("input2", "y:", y);
+        showValueInElement("output", "result:", z);
+    }, 0);
+})();
